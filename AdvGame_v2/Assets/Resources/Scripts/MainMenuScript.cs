4ef31711351bc0297fd75ca1bf0774a9e3e3b5da
Michael Gallagher <navysmeagol0175@gmail.com>
2022-11-12 10:20:17 -0500

    Barebones main menu with play and quit functionality

---
+++ AdvGame_v2/Assets/Resources/Scripts/MainMenuScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level, LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        if (Application.isEditor)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+
+            Application.Quit();
+        }
+    }
+}
