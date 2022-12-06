f2adaef7b1e526773ce93f62b112ec06c7ef2bf2
Michael Gallagher <navysmeagol0175@gmail.com>
2022-12-05 20:01:25 -0500

    Knockback script

---
+++ AdvGame_v2/Assets/Resources/Scripts/Knockback.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Knockback : MonoBehaviour
+{
+    // Class needs to be attached to the player prefab
+
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    public float strength = 17;
+    public float delay = 0.15f;
+
+    
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    public void PlayKnockBack(GameObject sender) 
+    {
+        StopAllCoroutines();
+        Vector2 direction = (transform.position - sender.transform.position).normalized;
+
+        rb.AddForce(direction * strength, ForceMode2D.Impulse);
+        StartCoroutine(Reset());
+
+    }
+
+    private IEnumerator Reset() 
+    {
+        yield return new WaitForSeconds(delay);
+        rb.velocity = Vector2.zero;
+        
+
+    }
+
+
+}
